2e62545d4614ac25bac926d894b2105e86c1b0c2
Corran <corranharker@hotmail.co.uk>
2015-05-24 00:58:39 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2824,7 +2824,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat('/me This bot was created by ' + botCreator + ', but is now maintained by ' + botMaintainer + ".");
+                        API.sendChat('/me This bot was created by ' + botCreator + ', but is now maintained by ' + botMaintainer + ' Helped by ' + botHelper + ".");
                     }
                 }
             },
