29d3ffc40285d1c69601833cbdbeaecc656ad756
Corran <corranharker@hotmail.co.uk>
2015-05-23 19:31:28 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2839,7 +2839,7 @@
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
                         var from = chat.un;
-                        var msg = '/me [@' + from + '] ';
+                        var msg = '/me @' + from + ' ';
 
                         msg += basicBot.chat.afkremoval + ': ';
                         if (basicBot.settings.afkRemoval) msg += 'ON';
