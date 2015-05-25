1d390897249dcee1829fce9b58ac2daf75882eec
Corran <corranharker@hotmail.co.uk>
2015-05-25 20:59:23 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2330,7 +2330,7 @@
 
             lockskipCommand: {
                 command: ['lockskip', 'ls'],
-                rank: 'bouncer',
+                rank: 'user',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
